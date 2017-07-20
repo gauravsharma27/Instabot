9139d1b727f5d64f74612f6147d56b5defa719cc
gaurav <sharmagauru123@gmail.com>
2017-07-21 00:37:18 +0530

    instabot

---
+++ constants.py
@@ -0,0 +1,6 @@
+#all global variable is here.
+
+#instagram api access token
+APP_ACCESS_TOKEN = '3519882872.9942843.e8b8b2aa369b4372ab67aa1fa823abfc'
+#instagram api base url
+BASE_URL = 'https://api.instagram.com/v1/'
